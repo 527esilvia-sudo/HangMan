535fc2de4082b878ef23b63f50dec7203c53ba4e
527esilvia-sudo <527esilvia@frhsd.com>
2026-03-27 16:46:29 +0000

    added blood works again but need to disable game at beginning

--- script.js
+++ script.js
@@ -126,29 +126,32 @@ function updateImage() {
   console.log('remainingGuesses:', remainingGuesses)
   let image;
   switch (remainingGuesses) {
-    case 7:
-      image = 'Untitled-8.png';
-      break;
-    case 6:
+    case 8:
       image = 'Untitled-1.png';
       break;
-    case 5:
+    case 7:
       image = 'Untitled-2.png';
       break;
-    case 4:
+    case 6:
       image = 'Untitled-3.png';
       break;
-    case 3:
+    case 5:
       image = 'Untitled-4.png';
       break;
-    case 2:
+    case 4:
       image = 'Untitled-5.png';
       break;
-    case 1:
+    case 3:
       image = 'Untitled-6.png';
       break;
-    case 0:
+    case 2:
       image = 'Untitled-7.png';
+      break;
+    case 1:
+      image = 'Untitled-8.png';
+      break;
+    case 0:
+      image = 'Untitled-9.png';
       break;
   }
   const hangmanImg = document.getElementById('stagesImg');
